9503ea8ac4b9e2e8f7e00d54764a0b51eafcda38
kaelsun <bpzxgzy@126.com>
2016-04-30 21:23:44 +0800

    add some fragment

--- src/com/zyguo/voicenote/MainActivity.java
+++ src/com/zyguo/voicenote/MainActivity.java
@@ -18,5 +18,30 @@ public class MainActivity extends FragmentActivity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+    
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+    
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+    
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+    
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+    
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 
 }
